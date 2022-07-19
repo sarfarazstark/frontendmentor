2013d5aaa651df0fc0a66afbd75af7fe0f582f39
sarfarazstark <sarfarazstark@gmail.com>
2022-07-19 14:02:07 +0530

    1st Challenge by jonas Schmedtmann

---
+++ Day 1/BMICalculator.js
@@ -0,0 +1,25 @@
+// Members data
+let MarkWeight = 78, MarkHeight = 1.69;
+let JohnWeight = 92, JohnHeight = 1.95;
+// Calculating mass
+
+let MarkMass = MarkWeight / (MarkHeight * MarkHeight);
+let  JohnMass = JohnWeight / (JohnHeight * JohnHeight);
+
+// Checking whether marks has a higher than John
+
+let markHigherBMI = MarkMass > JohnMass;
+console.log(markHigherBMI);
+
+// Members data
+let MarkWeight1 = 95, MarkHeight1 = 1.88;
+let JohnWeight1 = 85, JohnHeight1 = 1.76;
+// Calculating mass
+
+let MarkMass1 = MarkWeight1 / (MarkHeight1 * MarkHeight1);
+let  JohnMass1 = JohnWeight1 / (JohnHeight1 * JohnHeight1);
+
+// Checking whether marks has a higher than John
+
+let markHigherBMI1 = MarkMass1 > JohnMass1;
+console.log(markHigherBMI1);
