2edd9759929832697cbf534cbd6c28ba68886ee2
sarfarazstark <sarfarazstark@gmail.com>
2022-09-01 20:26:30 +0530

    Another change

--- Javascript/funda 2/script.js
+++ Javascript/funda 2/script.js
@@ -1,7 +1,25 @@
 'use strict';
-let userEx = false;
-const passTest = true;
+// let userEx = false;
+// const passTest = true;
 
-if(passTest) userEx = true;
+// if(passTest) userEx = true;
 
-if(userEx) console.log("its work :D");
+// if(userEx) console.log("its work :D");
+
+// function fruitProcessor(fruitName, fruitQuantity){
+//     const juice = `Total juice made of ${fruitName} is ${fruitQuantity * 50}gm`
+//     return juice;
+// }
+
+// console.log(fruitProcessor("Apple", 5));
+
+// Function Declaration Vs Expression
+
+// Function Declaration (can be call before defined)
+
+funcOne();
+function funcOne(){
+    console.log("Hello <-");
+}
+
+// Function Expression (cannot call before defined and used as value)
