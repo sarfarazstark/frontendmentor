748122fb1ecf5ab73af8da6dd2c7a70e42fc5c68
sarfarazstark <sarfarazstark@gmail.com>
2022-09-02 14:13:58 +0530

    Learn Arrow Function

--- Javascript/funda 2/script.js
+++ Javascript/funda 2/script.js
@@ -1,25 +1,43 @@
 'use strict';
 // let userEx = false;
 // const passTest = true;
-
 // if(passTest) userEx = true;
-
 // if(userEx) console.log("its work :D");
 
 // function fruitProcessor(fruitName, fruitQuantity){
 //     const juice = `Total juice made of ${fruitName} is ${fruitQuantity * 50}gm`
 //     return juice;
 // }
-
 // console.log(fruitProcessor("Apple", 5));
 
 // Function Declaration Vs Expression
-
 // Function Declaration (can be call before defined)
-
-funcOne();
-function funcOne(){
-    console.log("Hello <-");
-}
-
+// funcOne();
+// function funcOne(){
+//     console.log("Hello <-");
+// }
 // Function Expression (cannot call before defined and used as value)
+// const calculateAge = function (calculateAge){
+//     return 2048 - calculateAge;
+// }
+// const age = calculateAge(2002); 
+// console.log(age);
+
+// // Arrow Function
+// const calcAge = birthyear => 2048 - birthyear;
+// const age1 = calcAge(2002);
+// console.log(age1);
+
+// function calling function
+// function cutFruit(fruit){
+//     return fruit * 4;
+// }
+// function fruitProcessor(oranges, apples){
+//     const orangesPiece = cutFruit(oranges);
+//     const applesPiece = cutFruit(apples);
+//     const result =  `Total piece of Orange is ${orangesPiece} and Apples is ${applesPiece}`;
+//     return result;
+// }
+
+// console.log(fruitProcessor(4, 5));
+
