524bfe3dbaaef8b5996b1f1079ff0a90c99b6325
sarfarazstark <sarfarazstark@gmail.com>
2022-09-15 16:47:55 +0530

    JavaScript Fundamentals Clear ✅🎉

--- Javascript/funda 2/script.js
+++ Javascript/funda 2/script.js
@@ -336,27 +336,27 @@
 //     console.log('Loop is about to end...');
 //   }
 // }
-// #Coding Challenge #4
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
-const calcTip = function (bill) {
-  return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
-};
-for (let i = 0; i < bills.length; i++) {
-  tips.push(calcTip(bills[i]));
-  totals.push(bills[i] + tips[i]);
-}
-console.log(bills);
-console.log(tips);
-console.log(totals);
-const calcAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    // sum = sum + arr[i];
-    sum += arr[i];
-  }
-  return sum / arr.length;
-}
-console.log(calcAverage(totals));
-console.log(calcAverage(tips));
+// // #Coding Challenge #4
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+// const calcTip = function (bill) {
+//   return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+// };
+// for (let i = 0; i < bills.length; i++) {
+//   tips.push(calcTip(bills[i]));
+//   totals.push(bills[i] + tips[i]);
+// }
+// console.log(bills);
+// console.log(tips);
+// console.log(totals);
+// const calcAverage = function (arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     // sum = sum + arr[i];
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
+// }
+// console.log(calcAverage(totals));
+// console.log(calcAverage(tips));
