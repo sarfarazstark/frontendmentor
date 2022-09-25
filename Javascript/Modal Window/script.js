435636df15c8eca2827ee10ce7de2f03ec809e50
sarfarazstark <sarfarazstark@gmail.com>
2022-09-25 15:19:31 +0530

    Dice Game Project Completed ✅

--- Javascript/Modal Window/script.js
+++ Javascript/Modal Window/script.js
@@ -1,21 +1,29 @@
-/* eslint-disable semi */
-'use strict';
+/* eslint semi: [2] */
+/* eslint quotes: ["error", "double"] */
+"use strict";
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnShowModal = document.querySelectorAll('.show-modal');
-const btnCloseModal = document.querySelector('.close-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnShowModal = document.querySelectorAll(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
-for (let i = 0; i < btnShowModal.length; i++) btnShowModal[i].addEventListener('click', openModal);
+for (let i = 0; i < btnShowModal.length; i++) btnShowModal[i].addEventListener("click", openModal);
 
-btnCloseModal.addEventListener('click', closeModal)
-overlay.addEventListener('click', closeModal)
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    console.log(e.key);
+    closeModal();
+  }
+});
