b7ac013b38dbc64dd6cb5a0d58d7a73d7f81b441
sarfarazstark <sarfarazstark@gmail.com>
2023-11-05 18:04:07 +0530

    Form validation 👍

--- interactive-card-details-form-main/script.js
+++ interactive-card-details-form-main/script.js
@@ -1,22 +1,63 @@
 document.querySelector('#number').addEventListener('input', (e) => {
   e.target.value = e.target.value.replace(/\D/g, '').replace(/(\d{4})/g, '$1 ').trim();
 });
-const date = document.querySelectorAll('#date');
+const mm = document.querySelector('#mm');
+const yy = document.querySelector('#yy');
 const cvc = document.querySelector('#cvc');
 
-const preventText = function (input) {
-  input.addEventListener('input', (e) => {
+const defaultValue = {
+  name: 'Jane Appleseed',
+  number: '0000 0000 0000 0000',
+  mm: '00',
+  yy: '00',
+  cvc: '000'
+}
+
+
+const preventText = function (inputs) {
+  inputs.addEventListener('input', (e) => {
     e.target.value = e.target.value.replace(/\D/g, '');
   })
 }
 
-let dateArr = [];
+
 document.querySelectorAll('input').forEach((input) => {
   input.addEventListener('input', function(){
-      document.querySelector(`.${this.id}`).innerHTML = this.value;
+        document.querySelector(`.${this.id}`).innerHTML = this.value;
   })
 })
-console.log(dateArr);
-preventText(date[0]);
-preventText(date[1]);
+
+document.querySelector('button').addEventListener('click', function(){
+  document.querySelectorAll('input').forEach((inputs) => {
+    console.log(inputs);
+    const fieldDiv = inputs.parentElement;
+    let error = fieldDiv.querySelector('span');
+    if (!error) {
+      error = fieldDiv.appendChild(document.createElement('span'));
+    }
+    if(inputs.id === 'mm'){
+      inputs.style.borderColor = 'var(--red-input-error)';
+      return;
+    } else if(inputs.value.trim() === ''){
+      inputs.style.borderColor = 'var(--red-input-error)';
+      error.style.display = 'inline';
+      error.innerHTML = 'This field is required';
+    } else {
+      inputs.style.borderColor = 'var(--light-grayish-violet)';
+      error.remove();
+    }
+  })
+})
+
+document.querySelectorAll('input').forEach((input) => {
+  input.addEventListener('input', function(){
+    if(this.value.trim() === ''){
+      document.querySelector(`.${this.id}`).innerHTML = defaultValue[this.id];
+    }
+  })
+})
+
+
+preventText(mm);
+preventText(yy);
 preventText(cvc);
