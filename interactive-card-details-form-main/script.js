e68a173a96e13b4874da695d6bd2929659d9405e
sarfarazstark <sarfarazstark@gmail.com>
2023-11-05 18:42:56 +0530

    Pop up thanks alert done Debug some minor issues

--- interactive-card-details-form-main/script.js
+++ interactive-card-details-form-main/script.js
@@ -22,12 +22,22 @@ const preventText = function (inputs) {
 
 
 document.querySelectorAll('input').forEach((input) => {
-  input.addEventListener('input', function(){
-        document.querySelector(`.${this.id}`).innerHTML = this.value;
+  input.addEventListener('input', function () {
+    document.querySelector(`.${this.id}`).innerHTML = this.value;
   })
 })
 
-document.querySelector('button').addEventListener('click', function(){
+let allFilled = true;
+document.querySelector('button').addEventListener('click', function () {
+  const thanksAlert = document.querySelector('.thanks-alert');
+  if (thanksAlert.style.display === 'flex') {
+    thanksAlert.style.display = 'none';
+    document.querySelectorAll('.fields').forEach((fieldDiv) => {
+      fieldDiv.style.display = 'block';
+    });
+    return;
+  }
+
   document.querySelectorAll('input').forEach((inputs) => {
     console.log(inputs);
     const fieldDiv = inputs.parentElement;
@@ -35,27 +45,48 @@ document.querySelector('button').addEventListener('click', function(){
     if (!error) {
       error = fieldDiv.appendChild(document.createElement('span'));
     }
-    if(inputs.id === 'mm'){
+    if (inputs.id === 'mm' && inputs.value.trim() === '') {
       inputs.style.borderColor = 'var(--red-input-error)';
-      return;
-    } else if(inputs.value.trim() === ''){
+      allFilled = false;
+      return false;
+    } else if (inputs.value.trim() === '') {
+      document.querySelector(`.${inputs.id}`).innerHTML = defaultValue[inputs.id];
       inputs.style.borderColor = 'var(--red-input-error)';
       error.style.display = 'inline';
       error.innerHTML = 'This field is required';
+      allFilled = false;
     } else {
       inputs.style.borderColor = 'var(--light-grayish-violet)';
       error.remove();
     }
   })
+
+  if (allFilled) {
+    document.querySelectorAll('.fields').forEach((fieldDiv) => {
+      fieldDiv.style.display = 'none';
+    });
+    thanksAlert.style.display = 'flex';
+    allFilled = false;
+
+    // Clear input fields
+    document.querySelectorAll('input').forEach((input) => {
+      input.value = '';
+    });
+    // Reset aside values
+    Object.keys(defaultValue).forEach((key) => {
+      document.querySelector(`.${key}`).innerHTML = defaultValue[key];
+    });
+  }
 })
 
-document.querySelectorAll('input').forEach((input) => {
-  input.addEventListener('input', function(){
-    if(this.value.trim() === ''){
-      document.querySelector(`.${this.id}`).innerHTML = defaultValue[this.id];
-    }
-  })
-})
+
+// document.querySelectorAll('input').forEach((input) => {
+//   input.addEventListener('input', function () {
+//     if (this.value.trim() === '') {
+//       document.querySelector(`.${this.id}`).innerHTML = defaultValue[this.id];
+//     }
+//   })
+// })
 
 
 preventText(mm);
