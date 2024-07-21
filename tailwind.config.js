1d50e552a1b39ff2f9a964121076b1459492ceee
sarfarazstark <sarfarazstark@gmail.com>
2024-07-22 03:16:33 +0530

    Mobile version complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
 		// Add more project folders as needed
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				white: 'hsl(0, 0%, 100%)',
+				'fylo-blue-intro': 'hsl(217, 28%, 15%)',
+				'fylo-blue-main': 'hsl(218, 28%, 13%)',
+				'fylo-blue-footer': 'hsl(216, 53%, 9%)',
+				'fylo-blue-testimonials': 'hsl(219, 30%, 18%)',
+				'fylo-cyan-call-to-action': 'hsl(176, 68%, 64%)',
+				'fylo-blue-call-to-action': 'hsl(198, 60%, 50%)',
+				'fylo-light-red-error': 'hsl(0, 100%, 63%)',
+			},
+			fontFamily: {
+				body: ['Open sans', 'sans-serif'],
+				heading: ['Raleway', 'sans-serif'],
+			},
+			backgroundImage: {
+				'fylo-mobile-bg': "url('./images/bg-curvy-mobile.svg')",
+				'fylo-desktop-bg': "url('/images/bg-curvy-desktop.svg')",
+			},
+		},
 	},
 	plugins: [],
 };
