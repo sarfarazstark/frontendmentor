86244d071b3fd8b401890b3e1cd3c0cd1e8e4b92
sarfarazstark <sarfarazstark@gmail.com>
2024-07-21 20:02:49 +0530

    features: - Add Tailwind CSS configuration and global styles - fixed minor and big issues

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'**/*.{html,css,js}',
+		// Add more project folders as needed
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
