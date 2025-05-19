90a68a2cf779963f142a55ddb86fd0712e743731
sarfarazstark <sarfarazstark@gmail.com>
2025-05-19 08:16:17 +0530

    Multi Step Form Completed

--- multi-step-form-main/script.js
+++ multi-step-form-main/script.js
@@ -1,120 +1,280 @@
-const nextButtons = document.querySelectorAll("#next");
-const prevButtons = document.querySelectorAll("#prev");
-const radioInputs = document.querySelectorAll(
-  "section div ul li input[type='radio']"
-);
+const form = document.querySelector("form");
+// Aside
+const aside = document.querySelector(".steps");
 
-for (const next of nextButtons) {
-  next.addEventListener("click", (e) => {
-    e.preventDefault();
+const plans = {
+  monthly: {
+    arcade: 9,
+    advanced: 12,
+    pro: 15,
+  },
+  yearly: {
+    arcade: 90,
+    advanced: 120,
+    pro: 150,
+  },
+};
 
-    const section = e.target.parentElement.parentElement;
+const addOnsData = {
+  monthly: {
+    onlineService: 1,
+    largerStorage: 2,
+    customizableProfile: 2,
+  },
+  yearly: {
+    onlineService: 10,
+    largerStorage: 20,
+    customizableProfile: 20,
+  },
+};
 
-    const allInputs = section.querySelectorAll("div ul li input");
-
-    console.log(allInputs);
-
-    for (const input of allInputs) {
-      switch (input.type) {
-        case "text":
-          if (input.value === "") {
-            input.style.borderColor = "red";
-            return;
-          } else {
-            input.style.borderColor = "var(--light-gray)";
-          }
-          break;
-        case "email":
-          const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-          if (input.value === "" || !regex.test(input.value)) {
-            input.style.borderColor = "red";
-            return;
-          } else {
-            input.style.borderColor = "var(--light-gray)";
-          }
-          break;
-        case "radio":
-          const radioButtons = section.querySelectorAll("div ul li input");
-          let isChecked = false;
-
-          for (const radio of radioButtons) {
-            if (radio.checked) {
-              isChecked = true;
-              break;
-            }
-          }
-
-          if (!isChecked) {
-            radioButtons[0].nextElementSibling.style.borderColor = "red";
-            return;
-          } else {
-            radioButtons[0].nextElementSibling.style.outline = "none";
-          }
-          break;
-        case "checkbox":
-          const checkBoxes = section.querySelectorAll("input[type='checkbox']");
-          let isBoxChecked = false;
-
-          for (const box of checkBoxes) {
-            if (box.checked) {
-              isBoxChecked = true;
-              break;
-            }
-          }
-
-          if (!isBoxChecked) {
-            checkBoxes[0].style.outline = "1px solid red";
-            return;
-          } else {
-            checkBoxes[0].style.outline = "none";
-          }
-          break;
-        case "tel":
-          const regexTel =
-            /^\+\d{1,4}[\s\-]?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
-          if (input.value === "" || !regexTel.test(input.value)) {
-            input.style.borderColor = "red";
-            return;
-          } else {
-            input.style.borderColor = "var(--light-gray)";
-          }
-          break;
-        default:
-          break;
-      }
-    }
-
-    const nextSection = section.nextElementSibling;
-
-    section.classList.remove("show");
-    nextSection.classList.add("show");
+const changeStepBg = (step) => {
+  // Remove all active classes from li>div
+  const allSteps = document.querySelectorAll(".steps li > div");
+  allSteps.forEach((stepDiv) => {
+    stepDiv.classList.remove("active");
   });
-}
 
-prevButtons.forEach((prev) => {
-  prev.addEventListener("click", (e) => {
+  // Add active class to the current step
+  const currentStep = document.querySelector(
+    `.steps li:nth-child(${step}) > div`
+  );
+  currentStep.classList.add("active");
+};
+
+const prevBtn = document.querySelectorAll("section #prev");
+
+prevBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
     e.preventDefault();
+    const currentSection = e.target.closest("section");
+    const currentStep = parseInt(currentSection.classList[0].split("-")[1]);
 
-    const section = e.target.parentElement.parentElement;
-    const previous = section.previousElementSibling;
+    // Change background step
+    changeStepBg(currentStep - 1);
 
-    section.classList.remove("show");
-    previous.classList.add("show");
+    // Hide current section and show previous section
+    currentSection.classList.remove("show");
+    document
+      .querySelector(`section.step-${currentStep - 1}`)
+      .classList.add("show");
   });
 });
 
-radioInputs.forEach((radio) => {
-  radio.addEventListener("click", (e) => {
-    const radioButtons = e.target.parentElement.parentElement.querySelectorAll(
-      "input[type='radio']"
-    );
+// Step 1
+const stepOneInputs = document.querySelectorAll("section.step-1 input");
+const stepOneButton = document.querySelector("section.step-1 #next");
 
-    console.log(radioButtons);
+stepOneButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let allValid = true;
 
-    for (const button of radioButtons) {
-      button.nextElementSibling.style.borderColor = "none";
+  stepOneInputs.forEach((input) => {
+    // Previous sibling to show error message
+    const errorMessage = input.previousElementSibling;
+    if (!input.value) {
+      input.classList.add("show-error");
+
+      errorMessage.classList.add("show-error");
+
+      allValid = false;
+    } else {
+      input.classList.remove("show-error");
+      errorMessage.classList.remove("show-error");
     }
-
-    e.target.nextElementSibling.style.borderColor = "none";
   });
+
+  if (allValid) {
+    changeStepBg(2);
+    document.querySelector("section.step-1").classList.remove("show");
+    document.querySelector("section.step-2").classList.add("show");
+  }
+});
+
+// Step 2
+const step2 = document.querySelector("section.step-2");
+const planPrices = step2.querySelectorAll(".plan-price");
+const bonus = step2.querySelectorAll(".bonus");
+
+const step2Inputs = document.querySelectorAll("section.step-2 input");
+const step2Button = document.querySelector("section.step-2 #next");
+const planBtn = document.querySelector("section.step-2 .togglePlan");
+
+planBtn.addEventListener("click", (e) => {
+  // If checked yearly else monthly
+  const isYearly = e.target.checked;
+  const plan = isYearly ? "yearly" : "monthly";
+
+  // Change prices based on the plan
+  planPrices.forEach((price) => {
+    const priceValue = price.parentElement.parentElement.getAttribute(
+      `data-${plan}`
+    );
+    price.textContent = `$${priceValue}${isYearly ? "/yr" : "/mo"}`;
+  });
+
+  if (isYearly) {
+    bonus.forEach((b) => {
+      b.style.display = "block";
+    });
+
+    document.querySelector(".monthly").style.fontWeight = "normal";
+    document.querySelector(".yearly").style.fontWeight = "bold";
+  } else {
+    bonus.forEach((b) => {
+      b.style.display = "none";
+    });
+
+    document.querySelector(".yearly").style.fontWeight = "normal";
+    document.querySelector(".monthly").style.fontWeight = "bold";
+  }
+});
+
+step2Button.addEventListener("click", (e) => {
+  e.preventDefault();
+  let allValid = true;
+
+  // Check if any radio input is selected else show error
+  const radioInputs = step2.querySelectorAll("input[type='radio']");
+
+  if (![...radioInputs].some((input) => input.checked)) {
+    radioInputs.forEach((input) => {
+      input.nextElementSibling.classList.add("show-error");
+    });
+    allValid = false;
+  } else {
+    radioInputs.forEach((input) => {
+      input.nextElementSibling.classList.remove("show-error");
+    });
+  }
+
+  if (allValid) {
+    changeStepBg(3);
+    step2.classList.remove("show");
+    document.querySelector("section.step-3").classList.add("show");
+  }
+});
+
+// If user select a plan then remove errors
+const planInputs = step2.querySelectorAll("input[type='radio']");
+planInputs.forEach((input) => {
+  input.addEventListener("change", (e) => {
+    planInputs.forEach((input) => {
+      input.nextElementSibling.classList.remove("show-error");
+    });
+  });
+});
+
+// Step 3
+const step3Labels = document.querySelectorAll("section.step-3 label");
+const step3Button = document.querySelector("section.step-3 #next");
+let isYearly = false; // Default to yearly
+
+planBtn.addEventListener("click", (e) => {
+  // If checked yearly else monthly
+  isYearly = e.target.checked;
+
+  step3Labels.forEach((label) => {
+    const price = label.querySelector(".price");
+    const plan = isYearly ? "yearly" : "monthly";
+    const priceValue = label.getAttribute(`data-${plan}`);
+    price.textContent = `$${priceValue}${isYearly ? "/yr" : "/mo"}`;
+    // // Input values
+    // label.querySelector("input").value = priceValue;
+  });
+});
+
+step3Button.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  changeStepBg(4);
+  document.querySelector("section.step-3").classList.remove("show");
+  document.querySelector("section.step-4").classList.add("show");
+});
+
+// Step 4
+const step4Button = document.querySelector("section.step-4 #next");
+const plan = document.querySelector("section.step-4 .plan");
+const addOns = document.querySelector("section.step-4 .add-ons-list");
+const total = document.querySelector("section.step-4 .total");
+
+step3Button.addEventListener("click", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const selectedAddOns = formData.getAll("addons");
+  const data = Object.fromEntries(formData.entries());
+  const planSubscription =
+    data["plan-toggler"] === "yearly" ? "yearly" : "monthly";
+
+  // Render selected plan
+  const planH3 = plan.querySelector("h3");
+  const planPrice = plan.querySelector(".plan-price");
+  planH3.textContent = `${data.plan.toLocaleUpperCase()} (${
+    isYearly ? "Yearly" : "Monthly"
+  })`;
+
+  planPrice.textContent = `$${plans[planSubscription][data.plan]}${
+    isYearly ? "/yr" : "/mo"
+  }`;
+
+  // Render selected add-ons
+  addOns.innerHTML = ""; // Clear previous add-ons
+  let totalAddOnsPrice = 0;
+  selectedAddOns.forEach((addOn) => {
+    const addOnDiv = document.createElement("div");
+    addOnDiv.classList.add("add-on");
+    // Convert addOn name to camelCase to match object keys
+    const formatAddOnKey = (name) =>
+      name
+        .toLowerCase()
+        .replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) =>
+          index === 0 ? word.toLowerCase() : word.toUpperCase()
+        )
+        .replace(/\s+/g, "");
+
+    const addOnKey = formatAddOnKey(addOn);
+    const addOnPrice = addOnsData[planSubscription][addOnKey];
+    totalAddOnsPrice += addOnPrice;
+
+    addOnDiv.innerHTML = `
+      <div>
+        <p>${addOn}</p>
+        <span class="price">$${addOnPrice}${isYearly ? "/yr" : "/mo"}</span>
+      </div>
+    `;
+    addOns.appendChild(addOnDiv);
+  });
+
+  total.querySelector("p").textContent = `Total (per ${
+    isYearly ? "year" : "month"
+  })`;
+
+  // Update total (outside the loop)
+  total.querySelector(".total-price").textContent = `$${(
+    plans[planSubscription][data.plan] + totalAddOnsPrice
+  ).toFixed(2)}${isYearly ? "/yr" : "/mo"}`;
+});
+
+step4Button.addEventListener("click", (e) => {
+  e.preventDefault();
+  // Show thank you message
+  document.querySelector("section.step-4").classList.remove("show");
+  document.querySelector("section.thank-you").classList.add("show");
+
+  // Change background step
+  changeStepBg(5);
+});
+
+const changePlan = document.querySelector("#change-plan");
+
+changePlan.addEventListener("click", (e) => {
+  e.preventDefault();
+  // Show step 2
+  document.querySelector("section.step-2").classList.add("show");
+
+  // Remove show class from step 4
+  document.querySelector("section.step-4").classList.remove("show");
+
+  // Change background step
+  changeStepBg(2);
 });
