d207016d502c60a05eb14612aa1bf6e545240b73
sarfarazstark <sarfarazstark@gmail.com>
2025-10-18 01:15:10 +0530

    Fixesssssssss - Enhance JavaScript functionality in index.js for dynamic content loading - Improve image loading experience with placeholders and error handling

--- index.js
+++ index.js
@@ -1,52 +1,90 @@
+function getPathWithoutFile() {
+  const path = window.location.pathname;
+  const segments = path.split('/');
+  const lastSegment = segments[segments.length - 1];
+
+  if (lastSegment.includes('.')) {
+    segments.pop();
+  }
+
+  return segments.join('/') || '/';
+}
+
+const pathname = getPathWithoutFile();
+
 const upperCaseEveryWord = (word) =>
-	word
-		.split('-')
-		.filter((word) => word !== 'main' && word !== 'master')
-		.map((word) => `${word[0].toUpperCase() + word.slice(1)}`)
-		.join(' ');
+  word
+    .split('-')
+    .filter((word) => word !== 'main' && word !== 'master')
+    .map((word) => `${word[0].toUpperCase() + word.slice(1)}`)
+    .join(' ');
 
 const frontendmentor = (name, url) => {
-	let newName = name
-		.split('-')
-		.filter((word) => word !== 'main' && word !== 'master')
-		.join('-');
-	newName = newName.replace(/(\d)-/, '$1');
-	return newName + '-' + url;
+  let newName = name
+    .split('-')
+    .filter((word) => word !== 'main' && word !== 'master')
+    .join('-');
+  newName = newName.replace(/(\d)-/, '$1');
+  return newName + '-' + url;
 };
 
 const createListItemHTML = (item) => {
-	const { name, tech } = item;
-	const newName = upperCaseEveryWord(name);
-	return `<li>
-      <img src="/frontendmentor/${name}/design/desktop-preview.jpg" alt="${newName}">
-      <div class="tech">
-          ${tech
-						.split('-')
-						.map((name) => `<span class="${name}">${name}</span>`)
-						.join(' ')}
+  const { name, tech } = item;
+  const newName = upperCaseEveryWord(name);
+
+  return `
+   <li>
+    <div class="image-container">
+      <div class="image-placeholder">
+        <span class="placeholder-content">Loading...</span>
       </div>
-      <h3>${newName}</h3>
-      <div class="links">
-          <a href='https://www.frontendmentor.io/challenges/${frontendmentor(
-						name,
-						item.fmpage
-					)}'>Try out</a>
-          <a href='/frontendmentor/${name}'>Live</a>
-          <a href='https://github.com/sarfarazstark/frontendmentor/tree/main/${name}'>Repo</a>
-      </div>
-  </li>`;
+        <img
+          src="${pathname}${name}/design/desktop-preview.jpg"
+          alt="${newName}"
+          loading="lazy"
+          width="375"
+          height="200"
+          onload="this.classList.add('loaded'); this.previousElementSibling.style.opacity='0';"
+          onerror="this.classList.add('error'); this.previousElementSibling.innerHTML='<span class=\\'placeholder-content\\'>Failed to load</span>';"
+        />
+    </div>
+    <div class="tech">
+      ${tech
+        .split('-')
+        .map((name) => `<span class="${name}">${name}</span>`)
+        .join(' ')}
+    </div>
+    <h3>${newName}</h3>
+    <div class="links">
+      <a
+        class="link"
+        href="https://www.frontendmentor.io/challenges/${frontendmentor(
+          name,
+          item.fmpage
+        )}"
+        ><span>Challenge</span></a>
+      <a class="link" href="${pathname}${name}"
+        ><span>Live</span></a>
+      <a
+        class="link"
+        href="https://github.com/sarfarazstark/frontendmentor/tree/main/${name}"
+        ><span>Repo</span></a>
+    </div>
+  </li>
+`;
 };
+
 const main = document.querySelector('main');
 const li = main.appendChild(document.createElement('ul'));
 
 fetch('./data.json')
-	.then((response) => response.json())
-	.then((data) => {
-		data.sort((a, b) => new Date(a.submitDate) - new Date(b.submitDate));
-		for (const item of data) {
-			if ('status' in item) continue;
-			console.log(item);
-			li.insertAdjacentHTML('afterbegin', createListItemHTML(item));
-		}
-		document.querySelector('.total').textContent = data.length;
-	});
+  .then((response) => response.json())
+  .then((data) => {
+    data.sort((a, b) => new Date(a.submitDate) - new Date(b.submitDate));
+    for (const item of data) {
+      if ('status' in item) continue;
+      // console.log(item);
+      li.insertAdjacentHTML('afterbegin', createListItemHTML(item));
+    }
+    document.querySelector('.total').textContent = data.length;
+  });
