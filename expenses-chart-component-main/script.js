9e7e7eb6683f929b298d01ed60e5e251e8e826af
sarfarazstark <sarfarazstark@gmail.com>
2025-10-18 21:04:09 +0530

    feat: replace data.json import with inline data definition for chart rendering

--- expenses-chart-component-main/script.js
+++ expenses-chart-component-main/script.js
@@ -1,4 +1,33 @@
-import data from './data.json' assert { type: 'json' };
+const data = [
+  {
+    day: 'mon',
+    mount: 17.45,
+  },
+  {
+    day: 'tue',
+    amount: 34.91,
+  },
+  {
+    day: 'wed',
+    amount: 52.36,
+  },
+  {
+    day: 'thu',
+    amount: 31.07,
+  },
+  {
+    day: 'fri',
+    amount: 23.39,
+  },
+  {
+    day: 'sat',
+    amount: 43.28,
+  },
+  {
+    day: 'sun',
+    amount: 25.48,
+  },
+];
 
 const chart = document.querySelector('.chart');
 const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
@@ -6,7 +35,7 @@ const currentDay = days[new Date().getDay()];
 
 // Function to create bar HTML
 const createBarHTML = (item) => {
-	return `<div class='bar-container'>
+  return `<div class='bar-container'>
         <div class='bar'>
             <div class='bar__label'>${item.amount}</div>
             <div class='progress' data-size='${item.amount}' data-day='${item.day}'></div>
@@ -17,31 +46,31 @@ const createBarHTML = (item) => {
 
 // Function to handle mouseover event
 const handleMouseOver = (event) => {
-	const tooltip = event.target.previousElementSibling;
-	tooltip.style.display = 'block';
+  const tooltip = event.target.previousElementSibling;
+  tooltip.style.display = 'block';
 };
 
 // Function to handle mouseout event
 const handleMouseOut = (event) => {
-	const tooltip = event.target.previousElementSibling;
-	tooltip.style.display = 'none';
+  const tooltip = event.target.previousElementSibling;
+  tooltip.style.display = 'none';
 };
 
 // Insert bars into chart
 data.forEach((item) => {
-	const barHTML = createBarHTML(item);
-	chart.insertAdjacentHTML('beforeend', barHTML);
+  const barHTML = createBarHTML(item);
+  chart.insertAdjacentHTML('beforeend', barHTML);
 });
 
 // Select bars and apply styles
 const bars = document.querySelectorAll('div[data-size]');
 bars.forEach((bar) => {
-	bar.style.height = `${Number(bar.dataset.size) * 2 - 10}%`;
+  bar.style.height = `${Number(bar.dataset.size) * 2 - 10}%`;
 
-	if (currentDay === bar.dataset.day) {
-		bar.style.backgroundColor = 'hsl(186, 34%, 60%)';
-	}
+  if (currentDay === bar.dataset.day) {
+    bar.style.backgroundColor = 'hsl(186, 34%, 60%)';
+  }
 
-	bar.addEventListener('mouseover', handleMouseOver);
-	bar.addEventListener('mouseout', handleMouseOut);
+  bar.addEventListener('mouseover', handleMouseOver);
+  bar.addEventListener('mouseout', handleMouseOut);
 });
